c0aecd739834aba5b27a3afc5437855c9fe3ba0a
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2017-01-18 23:53:30 +0100

    example: upgrade to angular ^2.3.1

--- example/src/app/app.component.spec.ts
+++ example/src/app/app.component.spec.ts
@@ -1,21 +1,21 @@
 /* tslint:disable:no-unused-variable */
+
 import { TestBed, async } from '@angular/core/testing';
-
 import { AppComponent } from './app.component';
-import { AppModule } from './app.module';
 
-describe('App: Ng2GoogleChartsExample', () => {
+describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        AppModule
-      ]
+      declarations: [
+        AppComponent
+      ],
     });
+    TestBed.compileComponents();
   });
 
   it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 });
