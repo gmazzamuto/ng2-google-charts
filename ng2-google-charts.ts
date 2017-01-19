25c88e4f9bb5b755fd2e979149f1545aaf8f52f7
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2017-01-19 02:02:53 +0100

    Use ngm-cli and valor's project structure

--- ng2-google-charts.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/google-chart/google-chart.component';
-
-export { Ng2GoogleChartsModule } from './src/google-charts.module';
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './google-chart/google-chart.component';
+
+export { Ng2GoogleChartsModule } from './google-charts.module';
