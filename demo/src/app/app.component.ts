944a74f7967e54082ac6bb23045520b1bad7fe70
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2017-05-01 19:13:52 +0200

    demo: add combo chart with mouseover event

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ChartReadyEvent } from 'ng2-google-charts';
 import { ChartErrorEvent } from 'ng2-google-charts';
 import { ChartSelectEvent } from 'ng2-google-charts';
+import { DataPointHoveredEvent } from 'ng2-google-charts';
 
 @Component({
   selector: 'app-root',
@@ -97,6 +98,25 @@ export class AppComponent {
     options: {title: 'Company Performance'}
   };
 
+ public comboChartOptions:any =  {
+    chartType: 'ComboChart',
+    dataTable: [
+      ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
+      ['2004/05',  165,      938,         522,             998,           450,      614.6],
+      ['2005/06',  135,      1120,        599,             1268,          288,      682],
+      ['2006/07',  157,      1167,        587,             807,           397,      623],
+      ['2007/08',  139,      1110,        615,             968,           215,      609.4],
+      ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+    ],
+    options: {
+      title : 'Monthly Coffee Production by Country',
+      vAxis: {title: 'Cups'},
+      hAxis: {title: 'Month'},
+      seriesType: 'bars',
+      series: {5: {type: 'line'}}
+    }
+  };
+
  public myClick():void {
     // forces a reference update (otherwise angular doesn't detect the change)
     this.columnChartOptions = Object.create(this.columnChartOptions);
@@ -118,6 +138,13 @@ export class AppComponent {
 
   public select(event: ChartSelectEvent) {
     this.selectEvent = event;
+  }
+
+  public mouseOver(event: DataPointHoveredEvent) {
+    console.log('bb: ' + JSON.stringify(event.hoveredItemBoundingBox));
+    console.log('pos: ' + JSON.stringify(event.hoveredItemPosition));
+    console.log('type: ' + JSON.stringify(event.hoveredItemType));
+    console.log('value: ' + JSON.stringify(event.hoveredItemValue));
   }
 
 }
