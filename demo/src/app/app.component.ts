9409f1efc67f25a2265e3e8904ea469466e43273
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2019-01-18 19:51:14 +0100

    demo: add stacked column chart

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -48,6 +48,24 @@ export class AppComponent {
     }
   };
 
+  public stackedColumnChartData: any =  {
+    chartType: 'ColumnChart',
+    dataTable: [
+      ['Genre', 'Fantasy & Sci Fi', 'Romance', 'Mystery/Crime', 'General',
+       'Western', 'Literature', { role: 'annotation' } ],
+      ['2010', 10, 24, 20, 32, 18, 5, ''],
+      ['2020', 16, 22, 23, 30, 16, 9, ''],
+      ['2030', 28, 19, 29, 30, 12, 13, '']
+    ],
+    options: {
+      width: 600,
+      height: 400,
+      legend: { position: 'top', maxLines: 3 },
+      bar: { groupWidth: '75%' },
+      isStacked: true
+    }
+  };
+
   public pieChartData: any =  {
     chartType: 'PieChart',
     dataTable: [
