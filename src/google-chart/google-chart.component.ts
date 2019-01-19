389d93e9453bb1d82a61c767f1bc9233699adf37
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2019-01-19 18:12:49 +0100

    GoogleChartComponent: define options as empty dict if undefined
    Closes #47

--- src/google-chart/google-chart.component.ts
+++ src/google-chart/google-chart.component.ts
@@ -70,6 +70,9 @@ export class GoogleChartComponent implements OnChanges {
       }
 
       this.options = this.data.options;
+      if (!this.options) {
+        this.options = {};
+      }
 
       this.loaderService.load(this.data.chartType).then(() => {
         if(this.wrapper === undefined || this.wrapper.getChartType() !== this.data.chartType) {
