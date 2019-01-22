aaf5f3af15d36ed51d7454bd207b30e22e1d1d86
Giacomo Mazzamuto <g.mazzamuto@gmail.com>
2019-01-22 18:47:39 +0100

    demo: add example for AnnotationChart

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -300,6 +300,24 @@ export class AppComponent {
     }
   };
 
+  public annotationChart = {
+    chartType: 'AnnotationChart',
+    dataTable: [
+      ['Date', 'Kepler-22b mission', 'Kepler title', 'Kepler text', 'Gliese 163 mission', 'Gliese title',
+        'Gliese text'],
+      [new Date(2314, 2, 15), 12400, undefined, undefined, 10645, undefined, undefined],
+      [new Date(2314, 2, 16), 24045, 'Lalibertines', 'First encounter', 12374, undefined, undefined],
+      [new Date(2314, 2, 17), 35022, 'Lalibertines', 'They are very tall', 15766, 'Gallantors', 'First Encounter'],
+      [new Date(2314, 2, 18), 12284, 'Lalibertines', 'Attack on our crew!', 34334, 'Gallantors',
+        'Statement of shared principles'],
+      [new Date(2314, 2, 19), 8476, 'Lalibertines', 'Heavy casualties', 66467, 'Gallantors', 'Mysteries revealed'],
+      [new Date(2314, 2, 20), 0, 'Lalibertines', 'All crew lost', 79463, 'Gallantors', 'Omniscience achieved']
+    ],
+    options: {
+      displayAnnotations: true
+    }
+  };
+
   private orgChartCollapsed = false;
   private treeMapAppendCount = 0;
 
